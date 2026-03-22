2af85bc94079792c5f2999869c03f4bd43423e1c
ParamGhetia <paramgghetia@gmail.com>
2026-03-22 13:45:00 -0400

    moving and panning and visuals!

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 200809L
 #include "raylib.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,6 +11,7 @@ typedef struct Package {
   // Actual world location
   Vector2 location;
   Vector2 size;
+  int length;
   int capacity;
 } Package;
 
@@ -25,10 +27,24 @@ typedef struct HashMap {
   int count;
 } HashMap;
 
+/*
+#define TEXTBOX ((Color){255,49,46,255})
+#define BACKGROUND ((Color){27,31,118,255})
+#define WORDS ((Color){249,224,217,255})
+#define FONTIWANT "Heming.ttf"
+*/
+
+#define TEXTBOX ((Color){255,49,46,255})
+#define BACKGROUND ((Color){27,31,118,255})
+#define WORDS ((Color){249,224,217,255})
+#define FONTIWANT "JBM.ttf"
+
+Font myFont;
+
 int newBuffer(Package* curP);
 int writeBuffer(Package* curP, int input);
 void savePackage(Package* curP, PackageStorage* storray, Package* grid[][60]);
-void drawBuffer(char* buffer, Vector2 curPos);
+void drawBuffer(char* buffer, Vector2 curPos, int cellSize);
 int hash(int x, int y, int size);
 void expandHash(HashMap* hashmap);
 //cameraloc refers to the location or offset and cellsize is set by zooming
@@ -40,11 +56,13 @@ void deletePackage(HashMap* map, Vector2 location);
 int main(void)
 {
   InitWindow(1200, 1200, "zion");
+  myFont = LoadFontEx(FONTIWANT, 128, NULL, 0);
+  SetTextureFilter(myFont.texture, TEXTURE_FILTER_POINT);
 
   int key = 0;
   Package* packageGrid[60][60];
-  Vector2 lastPosition = {0.0f,0.0f};
-  Package curPack = {NULL,{0.0f,0.0f},{0,0},16};
+  //Vector2 lastPosition = {0.0f,0.0f};
+  Package curPack = {NULL,{0.0f,0.0f},{0,0}, 0, 16};
   PackageStorage storage = {NULL, 0, 16};
   Vector2 currentPosition = {0.0f, 0.0f};
   Vector2 cameraLocation = {0.0f, 0.0f};
@@ -58,36 +76,55 @@ int main(void)
   while (!WindowShouldClose())
   {
     BeginDrawing();
-    ClearBackground((Color){247, 246, 242, 255});
+    ClearBackground(BACKGROUND);
+
+    //DO THIS FIRST GNG VVVVV
+
+
+    //rendering is VERY SLOW AT HIGH STORED PACKAGES so maybe be nuanced with cals, if new package added just print that package, if mouse zooming or panning then call da renderer, otherwise DO NOT
+    //and then always just call drawbuffer since its very low cost and neccesary
+
+
     daRenderer(&storage, cameraLocation, cellsize);
+    drawBuffer(curPack.buffer, currentPosition, cellsize);
     key = GetCharPressed();
+
     if (key > 0)
     {
-        currentPosition = GetMousePosition();
-        if (!(currentPosition.x == lastPosition.x || currentPosition.y == lastPosition.x))
-        {
-                //write functiont o save current buffer
-            savePackage(&curPack, &storage, packageGrid);
-            free(curPack.buffer);
-            curPack.capacity = 16;
-            curPack.size.x = 0;
-            curPack.location = currentPosition;
-            if(newBuffer(&curPack) == 0)
-                exit(1);
-        }
+        //currentPosition.x = GetMousePosition().x / cellsize;
+        //currentPosition.y = GetMousePosition().y / cellsize;
+        //if (!(currentPosition.x == lastPosition.x || currentPosition.y == lastPosition.y))
         if(writeBuffer(&curPack, key)==0)
             exit(1);
-        lastPosition = currentPosition;
+        //lastPosition = currentPosition;
         //also draw at bottom left of screen
+    }
+    currentPosition.x = GetMousePosition().x / cellsize;
+    currentPosition.y = GetMousePosition().y / cellsize;
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+    {
+            //write functiont o save current buffer
+        curPack.location = currentPosition;
+        savePackage(&curPack, &storage, packageGrid);
+        printf("storage length: %d\n", storage.length);
+        free(curPack.buffer);
+        curPack.capacity = 16;
+        curPack.size.x = 0.0;
+        curPack.size.y = 0.0;
+        curPack.length = 0;
+        if(newBuffer(&curPack) == 0)
+            exit(1);
     }
     if (IsMouseButtonDown(MOUSE_BUTTON_MIDDLE))
     {
-      cameraLocation.x += GetMouseDelta().x / cellsize;
-      cameraLocation.y += GetMouseDelta().y / cellsize;
+      cameraLocation.x -= GetMouseDelta().x / cellsize;
+      cameraLocation.y -= GetMouseDelta().y / cellsize;
     }
-    cellsize += GetMouseWheelMove() * 20;
-    if(cellsize < 0)
-      cellsize = 0;
+    cellsize += GetMouseWheelMove() * 3;
+    if(cellsize < 5)
+      cellsize = 5;
+    if(cellsize > 100)
+      cellsize = 100;
     key = 0;
     EndDrawing();
    }
@@ -114,7 +151,7 @@ int newBuffer(Package* curP)
 
 int writeBuffer(Package* curP, int input)
 {
-  if(curP->size.x > (curP->capacity-1))
+  if(curP->length > (curP->capacity-1))
   {
     curP->capacity *= 2;
     char *temp = (char*)realloc(curP->buffer, curP->capacity);
@@ -130,9 +167,9 @@ int writeBuffer(Package* curP, int input)
   }
 
     //note this will incriment length AFTER this line runs
-  curP->buffer[curP->size.x++] = (char)input;
+  curP->buffer[curP->length++] = (char)input;
     //remember null character must be put at the end of string to signify it has ended.
-  curP->buffer[curP->size.x] = '\0';
+  curP->buffer[curP->length] = '\0';
 
   return 1;
 }
@@ -173,10 +210,16 @@ void savePackage(Package* curP, PackageStorage* storray, Package* grid[][60])
   }
 }
 
-void drawBuffer(char* buffer, Vector2 curPos)
+void drawBuffer(char* buffer, Vector2 curPos, int cellSize)
 {
-  DrawText(buffer, curPos.x, curPos.y, 20, ((Color){51,46,45,255}));
-  DrawText(buffer, 0, GetScreenHeight()-20, 20, ((Color){51,46,45,255}));
+  //happy accident the whole kepeing the buffer drawn eaxctly where cursor is every second until they release is fire
+  Vector2 mousePos = GetMousePosition();
+  DrawRectangle(mousePos.x, mousePos.y - cellSize/15, MeasureTextEx(myFont, buffer, cellSize, 1).x, cellSize, TEXTBOX);
+  DrawTextEx(myFont, buffer, mousePos, cellSize, 1, WORDS);
+
+  Vector2 bottomPos = {0.0f, GetScreenHeight() - cellSize};
+  DrawRectangle(bottomPos.x, bottomPos.y - cellSize/15, MeasureTextEx(myFont, buffer, cellSize, 1).x, cellSize, TEXTBOX);
+  DrawTextEx(myFont, buffer, bottomPos, cellSize, 1, WORDS);
 }
 
 void daRenderer(PackageStorage* pacStor, Vector2 cameraLoc, int cellSize)
@@ -184,14 +227,17 @@ void daRenderer(PackageStorage* pacStor, Vector2 cameraLoc, int cellSize)
   Vector2 screenRelPos;
   for (int i = 0; i < pacStor->length; i++)
   {
+    printf("rendering package: %s at %f %f\n", pacStor->storageArray[i]->buffer, cameraLoc.x, cameraLoc.y);
     screenRelPos.x = (pacStor->storageArray[i]->location.x - cameraLoc.x) * cellSize;
     screenRelPos.y = (pacStor->storageArray[i]->location.y - cameraLoc.y) * cellSize;
 
-    if (screenRelPos.x >= 0 && screenRelPos.x <= GetScreenWidth())
+    //righgt edge greater than 0?, left edge grearter than horizontal max?
+    if (screenRelPos.x + MeasureText(pacStor->storageArray[i]->buffer, cellSize) >= 0 && screenRelPos.x <= GetScreenWidth())
     {
       if (screenRelPos.y >= 0 && screenRelPos.y <= GetScreenHeight())
       {
-        DrawText(pacStor->storageArray[i]->buffer, screenRelPos.x, screenRelPos.y, cellSize, ((Color){51,46,45,255}));
+        DrawRectangle(screenRelPos.x, screenRelPos.y - cellSize/15, MeasureTextEx(myFont, pacStor->storageArray[i]->buffer, cellSize, 1).x, cellSize, TEXTBOX);
+        DrawTextEx(myFont, pacStor->storageArray[i]->buffer, screenRelPos, cellSize, 1, WORDS);
       }
     }
   }
@@ -217,7 +263,7 @@ void expandHash(HashMap* hashmap)
   //use size since we want to go through the entire array(if we going through all we have to check null or tombstone to make sure we dont pull a nonexistant package)
   for (int i = 0; i < hashmap->size/2; i++)
   {
-    if(!(middleman[i] == NULL || middleman[i] == (Package*)1))
+    if(!(middleman[i] == NULL || middleman[i] == TOMBSTONE))
       insertPackage(hashmap, middleman[i]);
   }
   free(middleman);
@@ -236,7 +282,7 @@ void insertPackage(HashMap* hashmap, Package* package)
     int i = index;
     do
     {
-      if (hashmap->hashArray[i] == NULL || hashmap->hashArray[i] == (Package*)1)
+      if (hashmap->hashArray[i] == NULL || hashmap->hashArray[i] == TOMBSTONE)
       {
         hashmap->hashArray[i] = package;
         hashmap->count++;
