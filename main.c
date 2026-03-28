3601def1706ebe072da84d9600ae1fdd45af78e4
ParamGhetia <paramgghetia@gmail.com>
2026-03-28 10:49:25 -0400

    relationships and persistence and id

--- main.c
+++ main.c
@@ -6,13 +6,29 @@
 
 #define TOMBSTONE (Package*)1
 
+typedef enum {
+  CHILD,
+  PARENT,
+  SPOUSE,
+}RelType;
+
+typedef struct Relationship {
+    RelType type;
+    int ID;
+    Vector2 relLocation;
+}Relationship;
+
 typedef struct Package {
   char* buffer;
+  int length;
+  int capacity;
   // Actual world location
   Vector2 location;
   Vector2 size;
-  int length;
-  int capacity;
+  int id;
+  Relationship* relationships;
+  int numRelationships;
+  int capacRelationships;
 } Package;
 
 typedef struct PackageStorage {
@@ -21,11 +37,36 @@ typedef struct PackageStorage {
   int capacity;
 } PackageStorage;
 
+typedef struct ModifiedPackage {
+  Package** modPackArray;
+  int length;
+  int capacity;
+} ModifiedPackage;
+
+typedef struct IndexEntry {
+  int id;
+  int fileOffset;
+} IndexEntry;
+
+typedef struct IndexArray {
+  IndexEntry* indexArray;
+  int length;
+  int capacity;
+} IndexArray;
+
 typedef struct HashMap {
   Package** hashArray;
   int size;
   int count;
 } HashMap;
+
+typedef struct IDPool {
+    int* returnedIDs;
+    int length;
+    int capacity;
+    int nextFresh;
+} IDPool;
+
 
 /*
 #define TEXTBOX ((Color){255,49,46,255})
@@ -34,38 +75,40 @@ typedef struct HashMap {
 #define FONTIWANT "Heming.ttf"
 */
 
-#define TEXTBOX ((Color){255,49,46,255})
-#define BACKGROUND ((Color){27,31,118,255})
-#define WORDS ((Color){249,224,217,255})
-#define FONTIWANT "JBM.ttf"
+#define TEXTBOX ((Color){242, 239, 233, 255})   // warm paper tone
+#define BACKGROUND ((Color){70, 52, 48, 255})   // muted cocoa
+#define WORDS ((Color){38, 32, 30, 255})        // deep ink
+#define FONTIWANT "Heming.ttf"
 
 Font myFont;
 
 int newBuffer(Package* curP);
 int writeBuffer(Package* curP, int input);
-void savePackage(Package* curP, PackageStorage* storray, Package* grid[][60]);
+void savePackage(Package* curP, PackageStorage* storray, HashMap* hashmap, IDPool* pool);
 void drawBuffer(char* buffer, Vector2 curPos, int cellSize);
 int hash(int x, int y, int size);
 void expandHash(HashMap* hashmap);
+IDPool newIDPool();
+int acquireID(IDPool* pool);
+void releaseID(IDPool* pool, int id);
 //cameraloc refers to the location or offset and cellsize is set by zooming
 void daRenderer(PackageStorage* pacStor, Vector2 cameraLoc, int cellSize);
 void insertPackage(HashMap* map, Package* package);
 Package* lookupPackage(HashMap* map, Vector2 location);
 void deletePackage(HashMap* map, Vector2 location);
 
-int main(void)
-{
+int main(void) {
   InitWindow(1200, 1200, "zion");
   myFont = LoadFontEx(FONTIWANT, 128, NULL, 0);
   SetTextureFilter(myFont.texture, TEXTURE_FILTER_POINT);
 
   int key = 0;
-  Package* packageGrid[60][60];
   //Vector2 lastPosition = {0.0f,0.0f};
-  Package curPack = {NULL,{0.0f,0.0f},{0,0}, 0, 16};
+  Package curPack = {NULL,{0.0f,0.0f},{0,0}, 0, 16, 0, NULL};
   PackageStorage storage = {NULL, 0, 16};
   Vector2 currentPosition = {0.0f, 0.0f};
   Vector2 cameraLocation = {0.0f, 0.0f};
+  IDPool idPool = newIDPool();
   int cellsize = 20;
 
   if(newBuffer(&curPack) == 0)
@@ -73,13 +116,15 @@ int main(void)
   //need to fill that null pointer with something before it starts.
   storage.storageArray = (Package**)malloc(sizeof(Package*)*storage.capacity);
 
-  while (!WindowShouldClose())
-  {
+  HashMap hashmap = {NULL, 16, 0};
+  hashmap.hashArray = (Package**)calloc(hashmap.size, sizeof(Package*));
+
+  while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BACKGROUND);
+    DrawFPS(0,10);
 
     //DO THIS FIRST GNG VVVVV
-
 
     //rendering is VERY SLOW AT HIGH STORED PACKAGES so maybe be nuanced with cals, if new package added just print that package, if mouse zooming or panning then call da renderer, otherwise DO NOT
     //and then always just call drawbuffer since its very low cost and neccesary
@@ -89,8 +134,7 @@ int main(void)
     drawBuffer(curPack.buffer, currentPosition, cellsize);
     key = GetCharPressed();
 
-    if (key > 0)
-    {
+    if (key > 0) {
         //currentPosition.x = GetMousePosition().x / cellsize;
         //currentPosition.y = GetMousePosition().y / cellsize;
         //if (!(currentPosition.x == lastPosition.x || currentPosition.y == lastPosition.y))
@@ -101,12 +145,10 @@ int main(void)
     }
     currentPosition.x = GetMousePosition().x / cellsize;
     currentPosition.y = GetMousePosition().y / cellsize;
-    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-    {
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
             //write functiont o save current buffer
         curPack.location = currentPosition;
-        savePackage(&curPack, &storage, packageGrid);
-        printf("storage length: %d\n", storage.length);
+        savePackage(&curPack, &storage, &hashmap, &idPool);
         free(curPack.buffer);
         curPack.capacity = 16;
         curPack.size.x = 0.0;
@@ -115,8 +157,7 @@ int main(void)
         if(newBuffer(&curPack) == 0)
             exit(1);
     }
-    if (IsMouseButtonDown(MOUSE_BUTTON_MIDDLE))
-    {
+    if (IsMouseButtonDown(MOUSE_BUTTON_MIDDLE)) {
       cameraLocation.x -= GetMouseDelta().x / cellsize;
       cameraLocation.y -= GetMouseDelta().y / cellsize;
     }
@@ -134,12 +175,39 @@ int main(void)
         return 0;
     }
 
-int newBuffer(Package* curP)
-{
+//ID Functions---------------------------------------------------------------------------------------------
+IDPool newIDPool() {
+    IDPool pool;
+    pool.returnedIDs = (int*)malloc(sizeof(int) * 16);
+    pool.length = 0;
+    pool.capacity = 16;
+    pool.nextFresh = 0;
+    return pool;
+}
+
+int acquireID(IDPool* pool) {
+    if (pool->length > 0)
+        return pool->returnedIDs[--pool->length];
+
+    return pool->nextFresh++;
+}
+
+void releaseID(IDPool* pool, int id) {
+    if (pool->length >= pool->capacity) {
+        pool->capacity *= 2;
+        int* temp = (int*)realloc(pool->returnedIDs, sizeof(int) * pool->capacity);
+        if (temp == NULL)
+            exit(1);
+        pool->returnedIDs = temp;
+    }
+    pool->returnedIDs[pool->length++] = id;
+}
+
+//Buffer Functions---------------------------------------------------------------------------------------------
+int newBuffer(Package* curP) {
   curP->buffer = (char*)malloc(sizeof(char) * curP->capacity);
 
-  if (curP->buffer == NULL)
-  {
+  if (curP->buffer == NULL) {
     printf("yo");
     return 0;
   }
@@ -149,18 +217,14 @@ int newBuffer(Package* curP)
   return 1;
 }
 
-int writeBuffer(Package* curP, int input)
-{
-  if(curP->length > (curP->capacity-1))
-  {
+int writeBuffer(Package* curP, int input) {
+  if(curP->length > (curP->capacity-1)) {
     curP->capacity *= 2;
     char *temp = (char*)realloc(curP->buffer, curP->capacity);
 
-    if(temp == NULL)
-    {
+    if(temp == NULL) {
         return 0;
     }
-
 
     curP->buffer = temp;
     //expand array to make room for hte next character
@@ -174,44 +238,7 @@ int writeBuffer(Package* curP, int input)
   return 1;
 }
 
-void savePackage(Package* curP, PackageStorage* storray, Package* grid[][60])
-{
-    Package* savePac = (Package*)malloc(sizeof(Package));
-
-  if(savePac == NULL)
-  {
-    exit(1);
-  }
-
-  memcpy(savePac, curP, sizeof(Package));
-  savePac->buffer = strdup(curP->buffer);
-
-  if(storray->length > (storray->capacity - 1))
-  {
-    storray->capacity *= 2;
-    Package** temp = (Package**)realloc(storray->storageArray, sizeof(Package*)*storray->capacity);
-
-    if(temp == NULL)
-    {
-      exit(1);
-    }
-
-    storray->storageArray = temp;
-  }
-  storray->storageArray[storray->length] = savePac;
-  storray->length++;
-
-  //I need to fix mouse click location adn mapping but lets do this first
-  for (int i = curP->location.x; i < (curP->location.x + curP->size.x); i++){
-    for (int j = curP->location.y; j < (curP->location.y + curP->size.y); j++){
-      //impliment expansion of these with realloc and malloc as it gets bigger but first for packageStorage array
-      grid[i][j] =  savePac;
-    }
-  }
-}
-
-void drawBuffer(char* buffer, Vector2 curPos, int cellSize)
-{
+void drawBuffer(char* buffer, Vector2 curPos, int cellSize) {
   //happy accident the whole kepeing the buffer drawn eaxctly where cursor is every second until they release is fire
   Vector2 mousePos = GetMousePosition();
   DrawRectangle(mousePos.x, mousePos.y - cellSize/15, MeasureTextEx(myFont, buffer, cellSize, 1).x, cellSize, TEXTBOX);
@@ -222,68 +249,52 @@ void drawBuffer(char* buffer, Vector2 curPos, int cellSize)
   DrawTextEx(myFont, buffer, bottomPos, cellSize, 1, WORDS);
 }
 
-void daRenderer(PackageStorage* pacStor, Vector2 cameraLoc, int cellSize)
-{
-  Vector2 screenRelPos;
-  for (int i = 0; i < pacStor->length; i++)
-  {
-    printf("rendering package: %s at %f %f\n", pacStor->storageArray[i]->buffer, cameraLoc.x, cameraLoc.y);
-    screenRelPos.x = (pacStor->storageArray[i]->location.x - cameraLoc.x) * cellSize;
-    screenRelPos.y = (pacStor->storageArray[i]->location.y - cameraLoc.y) * cellSize;
-
-    //righgt edge greater than 0?, left edge grearter than horizontal max?
-    if (screenRelPos.x + MeasureText(pacStor->storageArray[i]->buffer, cellSize) >= 0 && screenRelPos.x <= GetScreenWidth())
-    {
-      if (screenRelPos.y >= 0 && screenRelPos.y <= GetScreenHeight())
-      {
-        DrawRectangle(screenRelPos.x, screenRelPos.y - cellSize/15, MeasureTextEx(myFont, pacStor->storageArray[i]->buffer, cellSize, 1).x, cellSize, TEXTBOX);
-        DrawTextEx(myFont, pacStor->storageArray[i]->buffer, screenRelPos, cellSize, 1, WORDS);
-      }
+//Package Functions-----------------------------------------------------------------------------------------------
+void savePackage(Package* curP, PackageStorage* storray, HashMap* hashmap, IDPool* pool) {
+    Package* existing = lookupPackage(hashmap, curP->location);
+    if (existing != NULL) {
+        releaseID(pool, existing->id);
+        free(existing->buffer);
+        storray->storageArray[existing->id] = NULL;
+        deletePackage(hashmap, existing->location);
+        free(existing);
     }
-  }
+
+    Package* savePac = (Package*)malloc(sizeof(Package));
+    if (savePac == NULL)
+        exit(1);
+
+    memcpy(savePac, curP, sizeof(Package));
+    savePac->buffer = strdup(curP->buffer);
+    savePac->id = acquireID(pool);
+
+    // grow storageArray if the new id exceeds capacity
+    while (savePac->id >= storray->capacity) {
+        storray->capacity *= 2;
+        Package** temp = (Package**)realloc(storray->storageArray, sizeof(Package*) * storray->capacity);
+        if (temp == NULL)
+            exit(1);
+        storray->storageArray = temp;
+    }
+
+    storray->storageArray[savePac->id] = savePac;
+    if (savePac->id >= storray->length)
+        storray->length = savePac->id + 1;
+
+    insertPackage(hashmap, savePac);
 }
 
 
-int hash(int x, int y, int size)
-{
-  return (x * 19 + y) % size;
-}
-
-void expandHash(HashMap* hashmap)
-{
-  hashmap->size *= 2;
-  //cant do realloc i need to do malloc then free wiht a move in between
-  Package** temp = (Package**)malloc(sizeof(Package*) * hashmap->size);
-  if (temp == NULL)
-    exit(1);
-
-  Package** middleman = hashmap->hashArray;
-  hashmap->hashArray = temp;
-
-  //use size since we want to go through the entire array(if we going through all we have to check null or tombstone to make sure we dont pull a nonexistant package)
-  for (int i = 0; i < hashmap->size/2; i++)
-  {
-    if(!(middleman[i] == NULL || middleman[i] == TOMBSTONE))
-      insertPackage(hashmap, middleman[i]);
-  }
-  free(middleman);
-}
-
-
-void insertPackage(HashMap* hashmap, Package* package)
-{
-    if (hashmap->count/0.7 >= hashmap->size)
-    {
+void insertPackage(HashMap* hashmap, Package* package) {
+    if (hashmap->count/0.7 >= hashmap->size) {
       expandHash(hashmap);
     }
 
     int index = hash(package->location.x, package->location.y, hashmap->size);
     //do while executes at least once
     int i = index;
-    do
-    {
-      if (hashmap->hashArray[i] == NULL || hashmap->hashArray[i] == TOMBSTONE)
-      {
+    do {
+      if (hashmap->hashArray[i] == NULL || hashmap->hashArray[i] == TOMBSTONE) {
         hashmap->hashArray[i] = package;
         hashmap->count++;
         return;
@@ -295,14 +306,11 @@ void insertPackage(HashMap* hashmap, Package* package)
     insertPackage(hashmap, package);
 }
 
-Package* lookupPackage(HashMap* hashmap, Vector2 location)
-{
-
+Package* lookupPackage(HashMap* hashmap, Vector2 location) {
     int index = hash(location.x, location.y, hashmap->size);
     //do while executes at least once
     int i = index;
-    do
-    {
+    do {
       if(hashmap->hashArray[i] == NULL)
         return NULL;
       //need to check location to preven SEGFAULTING
@@ -315,17 +323,14 @@ Package* lookupPackage(HashMap* hashmap, Vector2 location)
     return NULL;
 }
 
-void deletePackage(HashMap* hashmap, Vector2 location)
-{
+void deletePackage(HashMap* hashmap, Vector2 location) {
   int index = hash(location.x, location.y, hashmap->size);
   //DOuu WHILE EXECUTESu AT least once
   int i = index;
-  do
-  {
+  do {
     if(hashmap->hashArray[i] == NULL)
       return;
-    else if ((hashmap->hashArray[i]->location.x == location.x) && (hashmap->hashArray[i]->location.y == location.y))
-    {
+    else if ((hashmap->hashArray[i]->location.x == location.x) && (hashmap->hashArray[i]->location.y == location.y)) {
       hashmap->hashArray[i] = (Package*)1;
       hashmap->count--;
       return;
@@ -333,4 +338,70 @@ void deletePackage(HashMap* hashmap, Vector2 location)
     //this wraps around the array. notice how we use size since we want the full size not count
     i = (i+1) % hashmap->size;
     } while(i != index);
+}
+
+//Renderer Functions---------------------------------------------------------------------------------------------
+void daRenderer(PackageStorage* pacStor, Vector2 cameraLoc, int cellSize) {
+  Vector2 screenRelPos;
+  for (int i = 0; i < pacStor->length; i++) {
+    //printf("rendering package: %s at %f %f\n", pacStor->storageArray[i]->buffer, cameraLoc.x, cameraLoc.y);
+    if (pacStor->storageArray[i] == NULL)  // add this
+        continue;
+
+    screenRelPos.x = (pacStor->storageArray[i]->location.x - cameraLoc.x) * cellSize;
+    screenRelPos.y = (pacStor->storageArray[i]->location.y - cameraLoc.y) * cellSize;
+
+    //righgt edge greater than 0?, left edge grearter than horizontal max?
+    if (screenRelPos.x + MeasureText(pacStor->storageArray[i]->buffer, cellSize) >= 0 && screenRelPos.x <= GetScreenWidth()) {
+      if (screenRelPos.y >= 0 && screenRelPos.y <= GetScreenHeight()) {
+        DrawRectangle(screenRelPos.x, screenRelPos.y - cellSize/15, MeasureTextEx(myFont, pacStor->storageArray[i]->buffer, cellSize, 1).x, cellSize, TEXTBOX);
+        DrawTextEx(myFont, pacStor->storageArray[i]->buffer, screenRelPos, cellSize, 1, WORDS);
+      }
+    }
+  }
+}
+
+//Hash Functions---------------------------------------------------------------------------------------------
+int hash(int x, int y, int size) {
+  return (x * 19 + y) % size;
+}
+
+void expandHash(HashMap* hashmap) {
+  hashmap->size *= 2;
+  //cant do realloc i need to do malloc then free wiht a move in between
+  Package** temp = (Package**)malloc(sizeof(Package*) * hashmap->size);
+  if (temp == NULL)
+    exit(1);
+
+  Package** middleman = hashmap->hashArray;
+  hashmap->hashArray = temp;
+
+  //use size since we want to go through the entire array(if we going through all we have to check null or tombstone to make sure we dont pull a nonexistant package)
+  for (int i = 0; i < hashmap->size/2; i++) {
+    if(!(middleman[i] == NULL || middleman[i] == TOMBSTONE))
+      insertPackage(hashmap, middleman[i]);
+  }
+  free(middleman);
+}
+
+//Data Functions---------------------------------------------------------------------------------------------
+void pullIndexes(){
+  put index.zn into array
+}
+
+void diskSave(ModifiedPackage* modPack) {
+  //Package and ID File Index Save
+  int fileIndex;
+  open data
+  for (int i = 0; i < modPack; i++)
+  { EVERYTIME I REFER TO INDEX I mean the array of indexes
+    fileIndex = search index.zn array version for modPack->modPackArray[i]->id
+    if file index found go there and turn on flag that says deleted
+    either way, append package to end of file and capture that index
+    go back to index array and insert sortedly, if the fileindex did exist then you just replace preexisting value if its new then you just inser new
+  }
+  close data
+  open index
+  delete index.zn and dump the array in a new one.
+  close index
 }
